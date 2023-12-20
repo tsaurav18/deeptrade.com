c005eff919a46e3dcc945d495b17be8bbaed4001
tsaurav18 <tsaurav1711@gmail.com>
2023-12-20 17:42:20 +0900

    add filtering

--- src/pages/XPercent/XPercent.js
+++ src/pages/XPercent/XPercent.js
@@ -185,6 +185,129 @@ function XPercent(props) {
             </div>
           </div>
         </div>
+
+
+{/* New row */}
+        {/* <div className="row g-0" style={{marginTop:"10px"}}>
+          <div className="col-12">
+            <div
+              className=
+                "xpercent_show"
+                  
+      
+            
+            >
+              <div className="row" style={{}}>
+                <div className="col-12 col-md-4"> 
+                  <div
+                    style={{
+                      textAlign: "center",
+                      backgroundColor: "#16334f",
+                      borderRadius: 5,
+                      color: "white",
+                      fontSize: 15,
+                      padding: "5px 0",
+                    }}
+                  >
+               ETF 관리형 자문서비스
+                  </div>
+                  <div className="row" style={{ marginTop: 10,  }}>
+                    <div
+                      className="col-3"
+                      style={{ display: "flex", alignItems: "center" }}
+                    >
+                      <img src="assets/partnership/DB_investment.png" alt="DB_investment" style={{width:"100%"}}/>
+                    </div>
+                    <div className="col-9 xpercent_text">
+                      리밸런싱 기간에 맞춘 적절한 ETF 비중 분배를 통해 안정적인 EMP를 구축할 수 있습니다. 
+                    </div>
+                  </div>
+                  <div className="row" style={{ marginTop: 10, padding:"0px 10px"}}>
+                    <div className="col-12" style={{textAlign:"center",backgroundColor:"#e7e7e7",borderRadius:4}}>
+                      <img
+                        src="assets/product/DB_investment.png"
+                        alt="DB_investment"
+                        style={{ width: "80%" }}
+                      />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className="col-12 col-md-8">
+                  <div
+                    className="signal_class"
+                    style={{
+                      textAlign: "center",
+                      backgroundColor: "#16334f",
+                      borderRadius: 5,
+                      color: "white",
+                      fontSize: 15,
+                      padding: "5px 0",
+                    }}
+                  >
+                  기업용 솔루션                  </div>
+                  <div className="row">
+                    <div className="col-12 col-md-6" style={{}}>
+                      <div className="row" style={{ marginTop: 10 }}>
+                        <div
+                          className="col-3"
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <img
+                            src="assets/product/circular_lines.png"
+                            alt="circular_lines"
+                            style={{ width: "60%" }}
+                          />
+                        </div>
+                        <div className="col-9 xpercent_text">
+                        국내외 거시 지표를 활용하여 경기 국면을 예측하고, 이를 적절한 자산 배분으로 연결합니다. 
+                        </div>
+                      </div>
+                      <div className="row" style={{ marginTop: 10 ,padding:"0px 10px"}}>
+                        <div className="col-12"style={{textAlign:"center", backgroundColor:"#e7e7e7",borderRadius:4}}>
+                          <img
+                            src="assets/product/graph_product_logo.png"
+                            alt="graph_product_logo"
+                            style={{ width: "79%", }}
+                          />
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div className="col-12 col-md-6">
+                      <div className="row" style={{ marginTop: 10 , }}>
+                        <div
+                          className="col-3"
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <img
+                            src="assets/product/koreainvestment_logo.jpeg"
+                            alt="koreainvestment_logo"
+                            style={{ width: "60%" }}
+                          />
+                        </div>
+                        <div className="col-9 xpercent_text">
+                          기업의 선호 조건에 부합하는 종목을 대상으로 상승확률이 높은 종목의 시그널을 제공합니다. 
+                        </div>
+                      </div>
+                      <div className="row" style={{ marginTop: 10,padding:"0px 10px"}}>
+                        <div className="col-12" style={{textAlign:"center",backgroundColor:"#e7e7e7", borderRadius:4}}>
+                          <img
+                            src="assets/partnership/korea_investment.png"
+                            alt="korea_investment"
+                            style={{ width: "79%" }}
+                          />
+                        </div>
+                      </div>
+                     
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
       </div>
     </div>
   );
